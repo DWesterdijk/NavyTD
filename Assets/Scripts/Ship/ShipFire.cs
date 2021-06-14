34db3018516ef5a16858ccdf1d731d08bc436350
Jarno van Lierop <24877@ma-web.nl>
2021-06-14 11:53:27 +0200

    feat: friendly ship reload

--- Assets/Scripts/Ship/ShipFire.cs
+++ Assets/Scripts/Ship/ShipFire.cs
@@ -11,6 +11,8 @@ public class ShipFire : MonoBehaviour
     [Header("Targets")]
     public List<GameObject> targets = new List<GameObject>();
 
+    ShipReload shipReload;
+
     public void Update()
     {
         if (targets.Count > 0)
@@ -23,8 +25,8 @@ public class ShipFire : MonoBehaviour
     private void Start()
     {
         EntityManager.current.AddTowersToList(this);
+        shipReload = gameObject.GetComponentInParent<ShipReload>();
     }
-
 
     public float totalCooldown;
 
@@ -32,7 +34,7 @@ public class ShipFire : MonoBehaviour
 
     private void Shoot()
     {
-        if (_shootCooldown <= 0)
+        if (_shootCooldown <= 0 && shipReload.ableToFire == true)
         {
             targets[0].GetComponent<GuiHealthBar>().currentHealth -= damage;
             Debug.Log("shot");
@@ -41,7 +43,7 @@ public class ShipFire : MonoBehaviour
                 targets.RemoveAt(0);
             }
 
-
+            shipReload.OnShipFire();
             _shootCooldown = totalCooldown;
         }
         else
